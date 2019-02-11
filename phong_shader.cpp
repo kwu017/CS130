d864795d3ff261f4bbae51d462d71223b7e935f4
kwu017 <kwu017@ucr.edu>
2019-02-10 21:28:13 -0800

    Update phong_shader.cpp

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -15,33 +15,38 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 ambient = color_ambient * world.ambient_intensity * world.ambient_color;
     vec3 diffuse = {0, 0, 0};
     vec3 specular = {0, 0, 0};
+    //vec3 lightvec;
+    vec3 shadowray;
+    vec3 viewray;
+    vec3 reflectray;
 
-    Ray shadowray;
+    //Ray shadowray;
     Light *current;
+    Ray shadowray_ray;
+    Hit intersection{};
     //Hit intersection{};
     //vec3 shadowrayvec;
 
+    
     for (unsigned i = 0; i < world.lights.size(); i++) {
     	current = world.lights.at(i);
+    	shadowray = current->position - intersection_point;
+    	shadowray_ray.endpoint = intersection_point;
+    	shadowray_ray.direction = (current->position - intersection_point).normalized();
+    	viewray = ray.endpoint - intersection_point;
+    	reflectray = 2 * dot(shadowray, normal) * normal - shadowray;
+    	intersection = world.Closest_Intersection(shadowray_ray);
 
-    	double diff_max = std::max(dot(normal.normalized(), -(intersection_point - (current->position)).normalized()), 0.0);
+    	if (!world.enable_shadows || (world.enable_shadows && (!intersection.object || intersection.dist >= shadowray.magnitude()))) {
+    		double diff_max = std::max(dot(normal.normalized(), -(intersection_point - (current->position)).normalized()), 0.0);
 
-    	vec3 l = current->position - intersection_point;
+    		vec3 l = current->position - intersection_point;
 
-    	double spec_max = pow(std::max(dot((intersection_point - ray.endpoint).normalized(), (l - 2 * dot(l, normal) * normal).normalized() ), 0.0), specular_power);
+    		double spec_max = pow(std::max(dot((intersection_point - ray.endpoint).normalized(), (l - 2 * dot(l, normal) * normal).normalized() ), 0.0), specular_power);
 
-    	diffuse = diffuse + color_diffuse * current->Emitted_Light(intersection_point - current->position) * diff_max;
-    	specular = specular + color_specular * current->Emitted_Light(intersection_point - current->position) * spec_max;
-    	/*shadowray.endpoint = intersection_point;
-    	shadowray.direction = (current->position - intersection_point).normalized();
-    	vec3 lightvec = current->position - intersection_point;*/
-
-    	/*if (world.Closest_Intersection(shadowray).object == NULL || (current->position - intersection_point).magnitude() < world.Closest_Intersection(shadowray).dist) {
-    		diffuse += color_diffuse * current->Emitted_Light(lightvec) * max(dot(shadowray.direction, normal), 0.0);
-    		vec3 view = -(ray.direction);
-    		vec3 reflect = ray.direction - 2 * (dot(ray.direction, normal)) * normal;
-    		specular += color_specular * current->Emitted_Light(lightvec) * pow(max(dot(view, reflect), 0.0), specular_power);
-    	}*/
+    		diffuse = diffuse + color_diffuse * current->Emitted_Light(intersection_point - current->position) * diff_max;
+    		specular = specular + color_specular * current->Emitted_Light(intersection_point - current->position) * spec_max;
+    	}
     }
 
     color = ambient + diffuse + specular;
